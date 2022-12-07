a9dc8a1aafe2d518d214cf0a23b7ed4092d4eb13
Christina Charles <charles03131@yahoo.com>
2022-12-06 19:19:07 -0600

    first commit

---
+++ greet/app.py
@@ -0,0 +1,14 @@
+
+from flask import flask
+
+app=(__name__)
+
+
+def welcome():
+    return f"Welcome"
+
+def welcome_home():
+    return f"Welcome Home"
+
+def welcome_back():
+    return f"Welcome Back"
