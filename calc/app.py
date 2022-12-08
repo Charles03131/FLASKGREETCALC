00f4752c8e38b427fac6f9b29bd0f547f74d92ca
Christina Charles <charles03131@yahoo.com>
2022-12-07 21:08:53 -0600

    updates

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,70 @@
 # Put your app in here.
+from flask import Flask,request
+from operations import add,subtract,mult,div
+
+app=Flask(__name__)
+
+
+
+@route('/add')
+def use_add():
+    """adds parameters a + b"""
+
+    a=int(request.args.get("a"))
+    b=int(request.args.get("b"))
+    answer=add(a,b)
+
+    return str(answer)
+
+
+
+
+
+@route('/sub')
+def use_sub():
+    """subtracts peramaters b from a"""
+
+    a=int(request.args.get("a"))
+    b=int(request.args.get("b"))
+    answer=sub(a,b)
+    
+    return str(answer)
+
+@route('mult')
+def use_mult():
+    """multiplies parameters a * b"""
+
+    a=int(request.args.get("a"))
+    b=int(request.args.get("b"))
+    answer=mult(a,b)
+
+    return str(answer)
+
+
+@route('div')
+def use_div():
+
+    a=int(request.args.get("a"))
+    b=int(request.args.get("b"))
+    answer=div(a,b)
+
+    return str(answer)
+
+
+
+operators={
+    "add":add,
+    "sub":sub,
+    "mult":mult,
+    "div":div,
+}
+
+@app.route("/math/<action>")
+def use_math(action):
+    """ use math ation on a and b"""
+
+    a=int(request.args.get("a"))
+    b=int(requst.args.get("b"))
+    answer=operators[action](a,b)
+
+    return str(result)
--- greet/app.py
+++ greet/app.py
@@ -1,14 +1,18 @@
 
-from flask import flask
-
-app=(__name__)
+from flask import Flask
 
 
+app=Flask(__name__)
+
+
+@app.route('/welcome')
 def welcome():
-    return f"Welcome"
+    return "welcome"
 
+@app.route('/welcome/home')
 def welcome_home():
-    return f"Welcome Home"
+   return "welcome home"
 
+@app.route('/welcome/back')
 def welcome_back():
-    return f"Welcome Back"
+    return "welcome back"
